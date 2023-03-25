debefe362dd6f6e5d69bbf3cd01da53347d84141
cjnour <charbel.nour9@gmail.com>
2023-03-25 13:29:06 -0400

    Edited App and index and initialized content.js

--- react-chrome-app/src/index.tsx
+++ react-chrome-app/src/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.createElement("div");
+rootElement.id = "react-chrome-app";
+
+const globalStyles = document.createElement("style");
+globalStyles.innerHTML = `
+  #${rootElement.id} {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 300px;
+  height: 100vh;
+  background: #ffffff;
+  border-right: 1px solid #c2c2c2;
+  z-index: 999;
+  }
+`;
+rootElement.appendChild(globalStyles);
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
